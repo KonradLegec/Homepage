efd9d73260bac1f16b5dcde5a7fdbd53be6acd3f
Konrad Legec <konrad.legec@gmail.com>
2022-09-05 23:28:05 +0200

    implemented functions in script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-let button = document.querySelector(".button");
-let body = document.querySelector(".body")
+{
+  const welcome = () => {
+    console.log("Witam wszystkich którzy zechcieli tutaj zajrzec");
+  };
 
-button.addEventListener("click", () => {
-    body.classList.toggle("body--blue")
+  const toggleBackground = () => {
+    const body = document.querySelector(".body");
+    body.classList.toggle("body--blue");
+  };
+
+  const toggleButton = () => {
+    const button = document.querySelector(".button");
     if (button.innerText === "Włącz zielone tło") {
-        button.innerText = "Włącz niebieskie tło"
-        button.classList.toggle("button--blue")
+      button.innerText = "Włącz niebieskie tło";
+      button.classList.toggle("button--blue");
     } else {
-        button.innerText = "Włącz zielone tło"
-        button.classList.toggle("button--blue")
+      button.innerText = "Włącz zielone tło";
+      button.classList.toggle("button--blue");
     }
+  };
 
-});
+  const init = () => {
+    const button = document.querySelector(".button");
+    button.addEventListener("click", toggleBackground);
+    button.addEventListener("click", toggleButton);
+
+    welcome();
+  };
+
+  init();
+}
